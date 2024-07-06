5feb9e3f0cb03b3dc5d7c59bf9462b59ab2dd133
yerong2 <yerong2@is-saltgpu.ischool.illinois.edu>
2024-07-05 21:32:54 -0500

    Demo for representation learning

---
+++ demo.py
@@ -0,0 +1,12 @@
+from repe import repe_pipeline_registry # register 'rep-reading' and 'rep-control' tasks into Hugging Face pipelines
+from transformers import pipeline
+from transformers import AutoModel, AutoTokenizer
+repe_pipeline_registry()
+
+# ... initializing model and tokenizer ....
+model_name = "microsoft/phi-2"
+model = AutoModel.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+rep_reading_pipeline =  pipeline("rep-reading", model=model, tokenizer=tokenizer)
+rep_control_pipeline =  pipeline("rep-control", model=model, tokenizer=tokenizer, **control_kwargs)
