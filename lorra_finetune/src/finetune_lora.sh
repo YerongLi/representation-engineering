7859d62c1ceec239a08e7359896a9eb7f7a0ef7e
yerong2 <yerong.li@outlook.com>
2024-08-23 03:28:37 -0500

    ## Checking Padding LEFT
     ==== Model merged successfully from checkpoint: ./output
    trainable params: 150,994,944 || all params: 8,817,835,008 || trainable%: 1.7123811441585095
    Loading data...
    Load 20 samples from ['data/only_text_example.json', '0.02']
    Load 10 samples from ['data/single_turn_single_image_example.json', '0.01']
    Load 10 samples from ['data/multi_turn_multi_images_example.json', '0.01']
    init mix data at rank 0
    load 20 data
    load 10 data
    load 10 data
    10samples is loaded
    True
      0%|                                                                                                         | 0/5 [00:00<?, ?it/s]Set seed 8 for rank 0
    /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/torch/utils/checkpoint.py:429: UserWarning: torch.utils.checkpoint: please pass in use_reentrant=True or use_reentrant=False explicitly. The default value of use_reentrant will be updated to be False in the future. To maintain current behavior, pass use_reentrant=True. It is recommended that you use use_reentrant=False. Refer to docs for more details on the differences between the two variants.
      warnings.warn(
    /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/torch/utils/checkpoint.py:61: UserWarning: None of the inputs have requires_grad=True. Gradients will be None
      warnings.warn(
    Could not estimate the number of tokens of the input, floating-point operations will not be computed
    /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/deepspeed/runtime/zero/stage_1_and_2.py:1586: UserWarning: The torch.cuda.*DtypeTensor constructors are no longer recommended. It's best to use methods such as torch.tensor(data, dtype=*, device='cuda') to create tensors. (Triggered internally at ../torch/csrc/tensor/python_tensor.cpp:83.)
      total_norm_cuda = get_accelerator().FloatTensor([float(total_norm)])
    {'loss': 0.526, 'learning_rate': 0.0002, 'epoch': 0.4}
    {'loss': 0.3005, 'learning_rate': 0.0002, 'epoch': 0.8}
    {'loss': 0.127, 'learning_rate': 0.0002, 'epoch': 1.2}
    {'loss': 0.0403, 'learning_rate': 0.0002, 'epoch': 1.6}
    {'loss': 0.0602, 'learning_rate': 0.0002, 'epoch': 2.0}
    {'train_runtime': 23.848, 'train_samples_per_second': 0.839, 'train_steps_per_second': 0.21, 'train_loss': 0.21081169992685317, 'epoch': 2.0}
    100%|█████████████████████████████████████████████████████████████████████████████████████████████████| 5/5 [00:23<00:00,  4.77s/it]
    /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/peft/utils/save_and_load.py:148: UserWarning: Could not find a config file in /home/yerong2/models/internlm-xcomposer2-vl-7b - will assume that the vocabulary was not modified.
      warnings.warn(
    [2024-08-23 03:27:33,368] [INFO] [launch.py:347:main] Process 2361090 exits successfully.

--- lorra_finetune/src/finetune_lora.sh
+++ lorra_finetune/src/finetune_lora.sh
@@ -2,13 +2,14 @@
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 DIR=`pwd`
 
-export MODEL="/home/yerong2/models/
-internlm-xcomposer2-7b"
-# export MODEL="merged/output"
-# export DATA="path of data"
+# export MODEL="internlm/internlm-xcomposer2-7b"
+# export MODEL="internlm/internlm-xcomposer2-vl-7b"
+# export MODEL="internlm/internlm-xcomposer2-4khd-7b"
+export MODEL="/home/yerong2/models/internlm-xcomposer2-vl-7b"
 export DATA="data.txt"
-ds_master_port=$((29000 + RANDOM % 1000))
-GPUS_PER_NODE=2
+# export DATA="path of data"
+export ds_master_port=$((29000 + RANDOM % 1000))
+GPUS_PER_NODE=8
 NNODES=1
 NODE_RANK=0
 MASTER_ADDR=localhost
@@ -21,36 +22,36 @@ DISTRIBUTED_ARGS="
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT
 "
-# deepspeed --num_gpus 2 finetune.py \
+
+# torchrun $DISTRIBUTED_ARGS finetune.py \
 deepspeed --master_port $ds_master_port --include localhost:1 finetune.py \
     --model_name_or_path $MODEL \
     --data_path $DATA \
+    --img_size 490 \
+    --hd_num -1 \
     --given_num True \
     --bf16 True \
     --fix_vit True \
     --fix_sampler True \
     --use_lora True \
-    --hd_num 18 \
     --output_dir output \
-    --num_train_epochs 10 \
+    --max_steps 5 \
     --batch_size 4 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "no" \
-    --save_steps 5 \
     --save_total_limit 1 \
-    --overwrite_output_dir \
-    --learning_rate 5e-5 \
+    --learning_rate 2e-4 \
     --weight_decay 0.1 \
     --adam_beta2 0.95 \
     --warmup_ratio 0.01 \
-    --lr_scheduler_type "cosine" \
+    --lr_scheduler_type "constant" \
     --logging_steps 1 \
     --report_to "none" \
-    --max_length 1024 \
+    --max_length 2048 \
     --deepspeed ds_config_zero2.json \
     --gradient_checkpointing True \
     --resume_from_checkpoint ./output
-    
+    # --num_train_epochs 1 \
