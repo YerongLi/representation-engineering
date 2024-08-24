d694ee34222e9835d01acd28c1c21f4996e53514
yerong2 <yerong.li@outlook.com>
2024-08-24 05:40:48 -0500

    Checkpoint : Pass the Lorra Argument /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/torch/utils/checkpoint.py:429: UserWarning: torch.utils.checkpoint: please pass in use_reentrant=True or use_reentrant=False explicitly. The default value of use_reentrant will be updated to be False in the future. To maintain current behavior, pass use_reentrant=True. It is recommended that you use use_reentrant=False. Refer to docs for more details on the differences between the two variants.   warnings.warn( /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/torch/utils/checkpoint.py:61: UserWarning: None of the inputs have requires_grad=True. Gradients will be None   warnings.warn( Could not estimate the number of tokens of the input, floating-point operations will not be computed /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/deepspeed/runtime/zero/stage_1_and_2.py:1586: UserWarning: The torch.cuda.*DtypeTensor constructors are no longer recommended. It's best to use methods such as torch.tensor(data, dtype=*, device='cuda') to create tensors. (Triggered internally at ../torch/csrc/tensor/python_tensor.cpp:83.)   total_norm_cuda = get_accelerator().FloatTensor([float(total_norm)]) {'loss': 2.4755, 'learning_rate': 0.0001, 'epoch': 0.4} {'loss': 2.9455, 'learning_rate': 9.045084971874738e-05, 'epoch': 0.8} {'loss': 1.2675, 'learning_rate': 6.545084971874738e-05, 'epoch': 1.2} {'loss': 1.9232, 'learning_rate': 3.4549150281252636e-05, 'epoch': 1.6} {'loss': 1.2356, 'learning_rate': 9.549150281252633e-06, 'epoch': 2.0} {'loss': 1.8464, 'learning_rate': 0.0, 'epoch': 2.4} {'train_runtime': 35.3546, 'train_samples_per_second': 1.697, 'train_steps_per_second': 0.17, 'train_loss': 1.9489455223083496, 'epoch': 2.4} 100%|█████████████████████████████████████████████████████████████████████████████| 6/6 [00:35<00:00,  5.89s/it] /home/yerong2/local/miniconda3/envs/mllm/lib/python3.11/site-packages/peft/utils/save_and_load.py:148: UserWarning: Could not find a config file in /home/yerong2/models/internlm-xcomposer2d5-7b - will assume that the vocabu

---
+++ lorra_finetune/src/migrate25/bak/single_finetune_lora.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+DIR=`pwd`
+CUDA_VISIBLE_DEVICES=2
+GPUS_PER_NODE=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+echo "==== NUMBER OF GPUS ==== GPUS_PER_NODE=$GPUS_PER_NODE"
+
+export MODEL="/home/yerong2/models/internlm-xcomposer2d5-7b"
+# export MODEL="merged/finetune_lora"
+OUTPUT_DIR="output"
+# export DATA="path of data"
+export DATA="data.txt"
+
+ds_master_port=$((29000 + RANDOM % 1000))
+
+
+NNODES=1
+NODE_RANK=0
+MASTER_ADDR=localhost
+MASTER_PORT=6001
+
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+rm output -rf
+# torchrun $DISTRIBUTED_ARGS finetune.py \
+deepspeed --include=localhost:1 finetune.py \
+    --model_name_or_path $MODEL \
+    --data_path $DATA \
+    --given_num True \
+    --bf16 True \
+    --fix_vit True \
+    --fix_sampler True \
+    --use_lora True \
+    --hd_num 18 \
+    --output_dir $OUTPUT_DIR \
+    --num_train_epochs 3 \
+    --batch_size 2 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "no" \
+    --save_steps 5 \
+    --save_total_limit 1 \
+    --overwrite_output_dir \
+    --learning_rate 1e-4 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
+    --warmup_ratio 0.01 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "none" \
+    --max_length 1024 \
+    --deepspeed ds_config_zero2.json \
+    --gradient_checkpointing True
